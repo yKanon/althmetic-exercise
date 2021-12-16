868edaf646d71499c9d5a0ab4c6e48252f0945e5
文三思 <394769450@qq.com>
2021-12-16 18:05:22 +0800

    feat: 142

---
+++ 力扣/142环形链表II.ts
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function detectCycle(head: ListNode | null): ListNode | null {
+  if(head === null) return null
+  let fast = head
+  let slow = head
+
+  while (fast !== null) {
+      slow = slow.next;
+      if (fast.next !== null) {
+          fast = fast.next.next;
+      } else {
+          return null;
+      }
+      if (fast === slow) {
+          let ptr = head;
+          while (ptr !== slow) {
+              ptr = ptr.next;
+              slow = slow.next;
+          }
+          return ptr;
+      }
+  }
+  return null;
+};
