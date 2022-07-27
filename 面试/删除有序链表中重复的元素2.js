1be626587020948a2a094910d36e82a6812584a8
yKanon <394769450@qq.com>
2022-07-27 08:52:52 +0800

    feat: 删除有序链表中重复的元素2

--- 面试/删除有序链表中重复的元素2.js
+++ 面试/删除有序链表中重复的元素2.js
@@ -1,9 +1,10 @@
-// 删除给出链表中的重复元素（链表中元素从小到大有序），使链表中的所有元素都只出现一次
+// 给出一个升序排序的链表，删除链表中的所有重复出现的元素，只保留原链表中只出现一次的元素。
 // 例如：
-// 给出的链表为1\to1\to21→1→2,返回1 \to 21→2.
-// 给出的链表为1\to1\to 2 \to 3 \to 31→1→2→3→3,返回1\to 2 \to 31→2→3.
+// 给出的链表为1 \to 2\to 3\to 3\to 4\to 4\to51→2→3→3→4→4→5, 返回1\to 2\to51→2→5.
+// 给出的链表为1\to1 \to 1\to 2 \to 31→1→1→2→3, 返回2\to 32→3.
 
-// 数据范围：链表长度满足 0 \le n \le 1000≤n≤100，链表中任意节点的值满足 |val| \le 100∣val∣≤100
+// 数据范围：链表长度 0 \le n \le 100000≤n≤10000，链表中的值满足 |val| \le 1000∣val∣≤1000
+// 要求：空间复杂度 O(n)O(n)，时间复杂度 O(n)O(n)
 // 进阶：空间复杂度 O(1)O(1)，时间复杂度 O(n)O(n)
 
 /*
@@ -20,18 +21,25 @@
  */
 function deleteDuplicates(head) {
   // write code here
-  if (head == null) return head;
+  if (!head || !head.next) return head;
 
-  let cur = head;
-  while (cur.next != null) {
-    if (cur.next.val == cur.val) {
-      cur.next = cur.next.next;
+  let dummy = new ListNode(0);
+  dummy.next = head;
+  let cur = dummy;
+
+  while (cur.next != null && cur.next.next != null) {
+    if (cur.next.val === cur.next.next.val) {
+      let temp = cur.next.val;
+
+      while (cur.next && cur.next.val === temp) {
+        cur.next = cur.next.next;
+      }
     } else {
       cur = cur.next;
     }
   }
 
-  return head;
+  return dummy.next;
 }
 module.exports = {
   deleteDuplicates: deleteDuplicates,
