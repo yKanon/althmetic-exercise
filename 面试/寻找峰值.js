0d7fe309917e1e736f3b9fd8ac328017aa09bb0b
yKanon <394769450@qq.com>
2022-08-08 18:45:32 +0800

    feat: 寻找峰值

---
+++ 面试/寻找峰值.js
@@ -0,0 +1,26 @@
+// 给定一个长度为n的数组nums，请你找到峰值并返回其索引。数组可能包含多个峰值，在这种情况下，返回任何一个所在位置即可。
+// 1.峰值元素是指其值严格大于左右相邻值的元素。严格大于即不能有等于
+// 2.假设 nums[-1] = nums[n] = -\infty−∞
+// 3.对于所有有效的 i 都有 nums[i] != nums[i + 1]
+// 4.你可以使用O(logN)的时间复杂度实现此问题吗？
+
+/**
+ * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+ *
+ *
+ * @param nums int整型一维数组
+ * @return int整型
+ */
+function findPeakElement(nums) {
+  // write code here
+  if (nums.length <= 1) return 0;
+  if (nums[0] > nums[1]) return 0;
+  if (nums[nums.length - 1] > nums[nums.length - 2]) return nums.length - 1;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > nums[i + 1] && nums[i] > nums[i - 1]) return i;
+  }
+}
+module.exports = {
+  findPeakElement: findPeakElement,
+};
